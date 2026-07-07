0a1e0fa74395a9d49862234e3da8d98a956421b4
mahedizaman <info.zaman.profile@gmail.com>
2026-07-07 12:55:02 +0600

    refactor: replace brand view stateless widget to statefull widget

--- lib/widgets/brand_view.dart
+++ lib/widgets/brand_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class BrandViewPage extends StatelessWidget {
+class BrandViewPage extends StatefulWidget {
   const BrandViewPage({super.key});
 
   @override
+  State<BrandViewPage> createState() => _BrandViewPageState();
+}
+
+class _BrandViewPageState extends State<BrandViewPage> {
+  int selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
     final List<String> brands = ['All', 'Adidas', 'Nike', 'Bata'];
+
     return SizedBox(
       height: 120,
       child: ListView.builder(
@@ -14,22 +21,32 @@ class BrandViewPage extends StatelessWidget {
         itemBuilder: (context, index) {
           final brand = brands[index];
 
-          final isSelected = index == 0;
+          final isSelected = selectedIndex == index;
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Chip(
-              label: Text(
-                brand,
-                style: TextStyle(
-                  color: isSelected ? Colors.white : Colors.black,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: Chip(
+                padding: EdgeInsets.all(12),
+                label: Text(
+                  brand,
+                  style: TextStyle(
+                    color: isSelected ? Colors.white : Colors.black,
+                  ),
                 ),
-              ),
-              backgroundColor: isSelected ? Colors.amber : Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50),
-                side: BorderSide(
-                  color: isSelected ? Colors.amber : Colors.grey,
-                  width: 1.5,
+                backgroundColor: isSelected ? Colors.amber : Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  side: BorderSide(
+                    color: isSelected
+                        ? Colors.amber
+                        : const Color.fromARGB(255, 231, 224, 224),
+                    width: 1,
+                  ),
                 ),
               ),
             ),
